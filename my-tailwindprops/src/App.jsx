5c014d2fd590e65a5e1eba83d6aa778f2634e7b3
Mudasir <apple.mudasir@gmail.com>
2026-08-23 12:02:55 +0700

    Add Card component and update App to use it

--- my-tailwindprops/src/App.jsx
+++ my-tailwindprops/src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className='bg-green-950'>
+
     Hello world!
   </h1>
+  <Card  name="shoe" title="welcome to mudasir shop" />
+  <Card name="shoe" title="welcome to mudasir shop" />
     </>
   )
 }
