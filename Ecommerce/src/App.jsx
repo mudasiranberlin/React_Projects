c0ccbae79ac7e5fd8aaa7e52ea3fd99c86f9a1ed
Mudasir <apple.mudasir@gmail.com>
2026-08-29 10:55:27 +0700

    feat: integrate react-router for navigation and add checkout route

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Startcode/Pages/Home'
+import { Route, Routes } from 'react-router'
 
 function App() {
 
   return (
     <>
-     <Home/>
+    <Routes>
+      <Route path='/' element={<Home/>} ></Route>
+       <Route path='checkout' element={<h1>Welcome to checkout page</h1>} ></Route>
+    </Routes>
+     
     </>
   )
 }
