1867f9a1f902ea4e43c0aade74bca5ab6416b797
Mudasir <apple.mudasir@gmail.com>
2026-08-29 11:41:08 +0700

    feat: add Orders and Tracking pages with responsive Header component

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Home from './Startcode/Pages/Home'
 import { Route, Routes } from 'react-router'
 import Checkout from './Startcode/Pages/Checkout'
+import Orders from './Startcode/Pages/Orders'
+import Tracking from './Startcode/Pages/Tracking'
 
 function App() {
 
@@ -11,6 +13,9 @@ function App() {
     <Routes>
       <Route index element={<Home/>} />
        <Route path='checkout' element={<Checkout/>} />
+        <Route path='orders' element={<Orders/>} />
+         <Route path='tracking' element={<Tracking/>} />
+        
     </Routes>
      
     </>
--- Ecommerce/src/Startcode/Pages/Checkout.jsx
+++ Ecommerce/src/Startcode/Pages/Checkout.jsx
@@ -1,32 +1,14 @@
 import React from 'react'
 import './checkout/checkout-header.css'
 import './checkout/checkout.css'
+import Header from '../../Componenets/Header'
 
 function Checkout() {
   return (
     <>
      <title>Checkout</title>
      
-    <div class="checkout-header">
-      <div class="header-content">
-        <div class="checkout-header-left-section">
-          <a href="/">
-            <img class="logo" src="images/logo.png" />
-            <img class="mobile-logo" src="images/mobile-logo.png" />
-          </a>
-        </div>
-
-        <div class="checkout-header-middle-section">
-          Checkout (<a class="return-to-home-link"
-            href="/">3 items</a>)
-        </div>
-
-        <div class="checkout-header-right-section">
-          <img src="images/icons/checkout-lock-icon.png" />
-        </div>
-      </div>
-    </div>
-
+    <Header/>
     <div class="checkout-page">
       <div class="page-title">Review your order</div>
 
