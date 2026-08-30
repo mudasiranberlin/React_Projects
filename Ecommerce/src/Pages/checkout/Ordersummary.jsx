ff6c0e8bf50165707da006f0c9d9b9d96b8022c7
Mudasir <apple.mudasir@gmail.com>
2026-08-30 13:02:24 +0700

    feat: implement delete functionality for cart items in Order Summary

--- Ecommerce/src/Pages/checkout/Ordersummary.jsx
+++ Ecommerce/src/Pages/checkout/Ordersummary.jsx
@@ -2,30 +2,37 @@ import dayjs from 'dayjs';
 import React from 'react'
 import { formatmoney } from '../../utils/money';
 import Deliveryoptions from './Deliveryoptions';
+import axios from 'axios';
 
-function Ordersummary({cart,deliveryOptions,loardCart}) {
+function Ordersummary({ cart, deliveryOptions, loardCart }) {
+
+
   return (
     <>
-    
-    <div class="order-summary">
-              {
-                 deliveryOptions.length > 0 && cart.map((cartItem)=>{
-                  const selectDeliverOption = deliveryOptions.find((deliveryOption)=>{
-                    return deliveryOption.id === cartItem.deliveryOptionId;
-                  })
-                  return(
-                    <div key={cartItem.productId} class="cart-item-container">
+
+      <div class="order-summary">
+        {
+          deliveryOptions.length > 0 && cart.map((cartItem) => {
+            const selectDeliverOption = deliveryOptions.find((deliveryOption) => {
+              return deliveryOption.id === cartItem.deliveryOptionId;
+            })
+            const deleteCartitem = async () => {
+              await axios.delete(`/api/cart-items/${cartItem.productId}`)
+              await loardCart();
+            }
+            return (
+              <div key={cartItem.productId} class="cart-item-container">
                 <div class="delivery-date">
                   Delivery date: {dayjs(selectDeliverOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                  
+
                 </div>
-    
+
                 <div class="cart-item-details-grid">
                   <img class="product-image"
                     src={cartItem.product.image} />
                   <div class="cart-item-details">
                     <div class="product-name">
-                     {cartItem.product.name}
+                      {cartItem.product.name}
                     </div>
                     <div class="product-price">
                       {formatmoney(cartItem.product.priceCents)}
@@ -37,21 +44,21 @@ function Ordersummary({cart,deliveryOptions,loardCart}) {
                       <span class="update-quantity-link link-primary">
                         Update
                       </span>
-                      <span class="delete-quantity-link link-primary">
+                      <span class="delete-quantity-link link-primary" onClick={deleteCartitem} >
                         Delete
                       </span>
                     </div>
                   </div>
 
-                  <Deliveryoptions deliveryOptions={deliveryOptions} cartItem={cartItem} loardCart={loardCart}/>
+                  <Deliveryoptions deliveryOptions={deliveryOptions} cartItem={cartItem} loardCart={loardCart} />
                 </div>
               </div>
-                  )
-    
-                })
-              }
-     
-            </div>
+            )
+
+          })
+        }
+
+      </div>
     </>
   )
 }
