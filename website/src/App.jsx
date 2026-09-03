54c550932cba24207cef730cb47c3431504201c4
Mudasir <apple.mudasir@gmail.com>
2026-09-03 09:57:27 +0700

    feat: add routing with react-router and integrate axios and dayjs dependencies

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,17 +3,19 @@ import Homepage from './Pages/Home/Homepage'
 import Checkout from './Pages/Checkout/Checkout'
 import Tracking from './Pages/order/Tracking'
 import Order from './Pages/order/Order'
+import {Route, Router,Routes} from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
-    <Homepage/>
-    <Checkout/>
-    <Tracking/>
-    <Order/>
+    <Routes>
+      <Route index element={<Homepage/>}/>
+      <Route path='checkout' element={<Checkout/>}/>
+      <Route path='orders' element={<Order/>}/>
+
+    </Routes>
       
     </>
   )
--- website/src/Pages/Home/Homepage.jsx
+++ website/src/Pages/Home/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './index.css'
-import './general.css'
 import './header.css'
 
 
