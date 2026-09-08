8ad3768db4dec92db6f3e1576bef6c8e42764b4e
Mudasir <apple.mudasir@gmail.com>
2026-09-08 13:21:37 +0700

    feat: add whatwg-url library with initial implementation
    - Introduced `utils.js` containing utility functions for object handling and property definitions.
    - Added `package.json` for the whatwg-url library, specifying dependencies, scripts, and metadata.
    - Created `webidl2js-wrapper.js` to export `URL` and `URLSearchParams` from the library.

---
+++ Backend/Models/user.model.js
@@ -0,0 +1,9 @@
+import mongoose, { Types } from "mongoose";
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        
+    }
+
+},{timestamps:true})
+export const User = mongoose.model("User",userSchema); 
