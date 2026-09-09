85d3d8cd1494b37cf182283941971840220f1a22
Mudasir <apple.mudasir@gmail.com>
2026-09-09 17:29:18 +0700

    feat: add cookie-parser and cors middleware, update MongoDB connection logging, and restructure app initialization

--- Backend2/src/db/index.js
+++ Backend2/src/db/index.js
@@ -4,7 +4,9 @@ import { DB_NAME } from "./constant.js"
 const connectDB = async () => {
     try {
         const connectionInstance = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`MongoDb is COnnected at !! DB_HOST:  ${connectionInstance.connection}`);
+        // console.log(`MongoDb is COnnected at !! DB_HOST:  ${connectionInstance.connection.host}`);
+        console.log(`MongoDB is connected at !! DB_HOST: ${mongoose.connection.host}`);
+        console.log("MONGODB_URI:", process.env.MONGODB_URI);
         
     } catch (error) {
         console.log("Mongo DB Connection ERROR",error);
--- Backend2/src/index.js
+++ Backend2/src/index.js
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at the Port : ${process.env.PORT}`);
+        
+    })
+
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed !!!",err);
+    
+})
