93387d703cc22fa01c0d9e6255c23ceef48192ac
Mudasir <apple.mudasir@gmail.com>
2026-09-12 12:10:46 +0700

    feat: implement user registration route and middleware for file uploads

--- Backend2/src/app.js
+++ Backend2/src/app.js
@@ -16,5 +16,17 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+
+//routes decleration 
+app.use("/api/v1/users",userRouter)
+
+
+// http://localhost:8000//api/v1/users/register
+
+// http://localhost:8000/users/login
 
 export default app
