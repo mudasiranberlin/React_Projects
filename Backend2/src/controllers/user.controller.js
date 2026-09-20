d4cc06f5abb56bb9a4bfe0a2181d463a627cb787
Mudasir <apple.mudasir@gmail.com>
2026-09-20 10:20:04 +0700

    Add getUserChannelProfile function to retrieve user channel details by username

--- Backend2/src/controllers/user.controller.js
+++ Backend2/src/controllers/user.controller.js
@@ -289,6 +289,20 @@ const updateUserCoverImage = asyncHandler(async (req,res) => {
 })
 
 
+const getUserChannelProfile = asyncHandler(async (req,res) => {
+    const {username} = req.params
+    if (!username?.trim()) {
+        throw new ApiError(400,"Please enter the username")
+    }
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        }
+    ])
+}) 
+
 
 
 export {
