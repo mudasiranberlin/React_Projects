e9343d66b734b86e4f86530a357b78c312d07fdb
Mudasir <apple.mudasir@gmail.com>
2026-09-20 10:56:41 +0700

    Enhance getUserChannelProfile to include subscription details and counts

--- Backend2/src/controllers/user.controller.js
+++ Backend2/src/controllers/user.controller.js
@@ -299,8 +299,58 @@ const getUserChannelProfile = asyncHandler(async (req,res) => {
             $match:{
                 username:username?.toLowerCase()
             }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as: "subscribers"
+            },
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as: "subscribedTo"
+            }
+        },{
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers",
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                email:1,
+                username:1,
+                subscribersCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1
+            }
         }
     ])
+    if (!channel?.length) {
+        throw new ApiError(404,"Channel does not Exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],"User channel fetched sucessfully")
+    )
 }) 
 
 
