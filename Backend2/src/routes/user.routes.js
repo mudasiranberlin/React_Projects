610c1656ecd7f38f71fd393fb226a8ed8ebec53f
Mudasir <apple.mudasir@gmail.com>
2026-09-22 11:43:40 +0700

    Add user profile and history routes: include cover image update and channel profile retrieval

--- Backend2/src/routes/user.routes.js
+++ Backend2/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAcessToken, registerUser, updateAccountDetails, updateUserAvatar } from "../controllers/user.controller.js"
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAcessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyjwt} from "../middlewares/auth.middleware.js"
 const router = Router()
@@ -30,6 +30,12 @@ router.route("/current-user").get(verifyjwt,getCurrentUser)
 router.route("/update-account").patch(verifyjwt,updateAccountDetails)
 
 router.route("/avatar").patch(verifyjwt,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-Image").patch(verifyjwt,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/username").get(verifyjwt,getUserChannelProfile)
+
+router.route("/history").get(verifyjwt,getWatchHistory)
 
 export default router
 
